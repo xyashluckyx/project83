5130108487b36079b534ea5465c89c2c169123f3
xyashluckyx <77104518+xyashluckyx@users.noreply.github.com>
2022-06-06 10:50:59 +0530

    Add files via upload

---
+++ screens/Feed.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import {View,Text,StyleSheet,SafeAreaView,Platform,StatusBar,Image,FlatList } from "react-native";
+
+import { RFValue } from "react-native-responsive-fontsize";
+import PostCard from './PostCard';
+
+let posts = require("./temp_posts.json");
+
+export default class Feed extends Component{
+    renderItem = ({ item: post }) => {
+      return <PostCard post={post} />;
+    };
+  
+    keyExtractor = (item, index) => index.toString();
+    
+    render(){
+        return(
+            <View style={styles.container}>
+                <SafeAreaView style={styles.droidSafeArea} />
+                <View style={styles.appTitle}>
+                    <View style={styles.appTitle}>
+                        <Image
+                            source={require('../assets/logo.png')}
+                            style={styles.iconImage}
+                        ></Image>
+                    </View>
+                    <View style={styles.appTitleTextContainer}>
+                        <Text style={styles.appTitleText}>Spectagram</Text>
+                    </View>
+                </View>
+                <View>
+                    <FlatList
+                        keyExtractor={this.keyExtractor}
+                        data={posts}
+                        renderItem={this.renderItem}
+                    />
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "black"
+    },
+    droidSafeArea: {
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+    },
+    appTitle: {
+        flex: 0.07,
+        flexDirection: "row"
+    },
+    appIcon: {
+        flex: 0.2,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    iconImage: {
+        width: "100%",
+        height: "100%",
+        resizeMode: "contain"
+    },
+    appTitleTextContainer: {
+        flex: 0.8,
+        justifyContent: "center"
+    },
+    appTitleText: {
+        color: "white",
+        fontSize: RFValue(28)
+    }
+})
+
+
